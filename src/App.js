6ba05170cb381f242a046b73b6df2496f13b9fd4
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-04-07 20:51:47 +0200

    added personal links

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-   return <div className="App">Weather app</div>;
+   return (
+      <div className="App">
+         <h1> Weather app </h1>
+         <p>
+            This project was coded by{" "}
+            <a href="https://github.com/di-legorreta"> Diana Legorreta </a> and
+            is open sourced on{" "}
+            <a
+               href="https://github.com/di-legorreta/react-final-project"
+               target="_blank"
+            >
+               github
+            </a>{" "}
+         </p>
+      </div>
+   );
 }
