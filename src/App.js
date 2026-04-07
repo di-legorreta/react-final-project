069063ec4e5bb075c2079e9d413d44b9b14a8aa2
di-legorreta <114834384+di-legorreta@users.noreply.github.com>
2026-04-07 20:58:30 +0200

    updated links

--- src/App.js
+++ src/App.js
@@ -7,11 +7,19 @@ export default function App() {
          <h1> Weather app </h1>
          <p>
             This project was coded by{" "}
-            <a href="https://github.com/di-legorreta"> Diana Legorreta </a> and
-            is open sourced on{" "}
+            <a
+               href="https://github.com/di-legorreta"
+               target="_blank"
+               rel="noopener"
+            >
+               {" "}
+               Diana Legorreta{" "}
+            </a>{" "}
+            and is open sourced on{" "}
             <a
                href="https://github.com/di-legorreta/react-final-project"
                target="_blank"
+               rel="noopener"
             >
                github
             </a>{" "}
